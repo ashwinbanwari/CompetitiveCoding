edc53c611b4ab8a821bc5d56c7e3ad93012c6b55
Ashwin Kishin Banwari <ashwinkbanwari@gmail.com>
2020-11-02 22:32:09 -0800

    WORKING BAT2

--- Practice/CommonLounge/DynamicProgramming/BAT2.cpp
+++ Practice/CommonLounge/DynamicProgramming/BAT2.cpp
@@ -10,31 +10,32 @@
 
 using namespace std;
 
-long n;
+long long n;
 
-long ans (long ind, long x, long y, vector<vector<vector<long>>>& dp, vector<long>& a){
+long long ans (long long ind, long long x, long long y, vector<vector<vector<long long>>>& dp, vector<long long>& a){
     if(ind == n + 1) return 0;
     if(dp[ind][x][y] != -1) return dp[ind][x][y];
-    long ret = ans(ind+1,x,y, dp, a);
-    if(x == 0) ret = max(ret,1 + ans(ind+1,ind,y, dp, a));     // chose the 1st element  belonging to the increasing seq.
-    if(y == 0) ret = max(ret,1 + ans(ind+1,x,ind, dp, a));     // chose the 1st element  belonging to the decreasing seq.
+    long long ret = ans(ind+1,x,y, dp, a);
+    if(x == 0) ret = max(ret,1 + ans(ind+1,ind,y, dp, a));     // chose the 1st element  belong longing to the increasing seq.
+    if(y == 0) ret = max(ret,1 + ans(ind+1,x,ind, dp, a));     // chose the 1st element  belong longing to the decreasing seq.
     if(a[ind] > a[x]) ret = max(ret,1 + ans(ind+1,ind,y, dp, a));
     if(a[ind] < a[y]) ret = max(ret,1 + ans(ind+1,x,ind, dp, a));
     return dp[ind][x][y] = ret;
 }
-void solve(vector<long>& rooms) {
-    vector<vector<vector<long>>> dp(rooms.size()+1, vector<vector<long>>(rooms.size()+1, vector<long>(rooms.size()+1, -1)));
+
+void solve(vector<long long>& rooms) {
+    vector<vector<vector<long long>>> dp(n+1, vector<vector<long long>>(n+1, vector<long long>(n+1, -1)));
     cout << ans(1, 0, 0, dp, rooms) << endl;
 }
 
 
 int main() {
-    long t;
+    long long t;
     cin >> t;
     while (t--) {
         cin >> n;
-        vector<long> in(n);
-        for (long i = 0; i < n; i++) {
+        vector<long long> in(n+1);
+        for (long long i = 1; i <= n; i++) {
             cin >> in[i];
         }
         solve(in);
